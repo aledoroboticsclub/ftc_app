345121aadbf9016c24ea6201badb48263c0cd052
ALEDO\jreynoldsstudent <aledoroboticsclub@gmail.com>
2017-10-30 23:58:51 -0500

    Added the robot class file and a basic teleop file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code9161_2017/Strider.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode.Code9161_2017;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Created by Aledo HS Robotics on 4/12/2017.
+ */
+
+public class Strider
+{
+
+	DcMotor frontLeft;
+	DcMotor frontRight;
+	DcMotor backLeft;
+	DcMotor backRight;
+	Servo spinner1;
+	Servo spinner2;
+
+	private ElapsedTime runtime=new ElapsedTime();
+
+	public void initRobot(HardwareMap spareMap, Telemetry tempTelemetry)
+	{
+		Telemetry telemetry=tempTelemetry;
+		frontLeft=spareMap.dcMotor.get("front left wheel");
+		frontRight=spareMap.dcMotor.get("front right wheel");
+		backLeft=spareMap.dcMotor.get("back left wheel");
+		backRight=spareMap.dcMotor.get("back right wheel");
+		frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+		backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+		spinner1=spareMap.servo.get("spinner 1");
+		spinner2=spareMap.servo.get("spinner 2");
+	}
+
+	public void setToForward ()
+	{
+		frontLeft.setPower(1);
+		frontRight.setPower(1);
+		backLeft.setPower(1);
+		backRight.setPower(1);
+	}
+
+	public void setToBackward ()
+	{
+		frontLeft.setPower(-1);
+		frontRight.setPower(-1);
+		backLeft.setPower(-1);
+		backRight.setPower(-1);
+	}
+
+	public void setToClockwise ()
+	{
+		frontLeft.setPower(1);
+		frontRight.setPower(-1);
+		backLeft.setPower(1);
+		backRight.setPower(-1);
+	}
+
+	public void setToCounterclockwise ()
+	{
+		frontLeft.setPower(-1);
+		frontRight.setPower(1);
+		backLeft.setPower(-1);
+		backRight.setPower(1);
+	}
+
+	public void setToStill ()
+	{
+		frontLeft.setPower(0);
+		frontRight.setPower(0);
+		backLeft.setPower(0);
+		backRight.setPower(0);
+	}
+
+	public void setToLeft ()
+	{
+		frontLeft.setPower(-1);
+		frontRight.setPower(1);
+		backLeft.setPower(1);
+		backRight.setPower(-1);
+	}
+
+	public void setToRight ()
+	{
+		frontLeft.setPower(1);
+		frontRight.setPower(-1);
+		backLeft.setPower(-1);
+		backRight.setPower(1);
+	}
+
+	public void runIntake ()
+	{
+		spinner1.setPosition(1);
+		spinner2.setPosition(0);
+	}
+
+	public void stopIntake ()
+	{
+		spinner1.setPosition(.5);
+		spinner2.setPosition(.5);
+	}
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code9161_2017/TeleopStrider.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.Code9161_2017;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+
+/**
+ * Created by Aledo HS Robotics on 4/12/2017.
+ * gamepad1-
+ * 			tank drive with joysticks
+ * 			strafe with dpad
+ *			precise control mode while holding down left bumper
+ * gamepad2-
+ * 			pulley control with dpad, up for up, down for down
+ * 			flicker control with triggers, launch with right
+ * 			beaconPusher control with x and b, x for left, b for right
+ * 			collector control with a and y, a for take in, y for spit out
+ * 			cap ball container control with dpad, left for release, right for holding
+ */
+
+@TeleOp(name="TeleopStrider ", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
+//@Disabled     //Determines if the program shows up on Driver Station
+
+
+public class TeleopStrider extends OpMode
+{
+	Strider r=new Strider();
+	public void init()
+	{
+		r.initRobot(hardwareMap,telemetry);
+		telemetry.addData("init Complete","");
+		telemetry.update();
+	}
+
+	public void initLoop(){}
+
+	public void start(){}
+
+	public void loop()
+	{
+		if(gamepad1.left_stick_y>.25)
+		{
+			r.setToForward();
+		}
+		else if(gamepad1.left_stick_y<-.25)
+		{
+			r.setToBackward();
+		}
+		else if(gamepad1.right_stick_x<-.25)
+		{
+			r.setToCounterclockwise();
+		}
+		else if(gamepad1.right_stick_x>.25)
+		{
+			r.setToClockwise();
+		}
+		else if(gamepad1.left_stick_x>.25)
+		{
+			r.setToRight();
+		}
+		else if(gamepad1.left_stick_x<-.25)
+		{
+			r.setToLeft();
+		}
+		else
+		{
+			r.setToStill();
+		}
+
+		if(gamepad1.a == true)
+		{
+			r.runIntake();
+		}
+		else if(gamepad1.a == false)
+		{
+			r.stopIntake();
+		}
+	}
+}
